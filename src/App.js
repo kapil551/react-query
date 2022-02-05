a37723af022bc02e165c127f4129d0c77c2dbd69
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-05 22:42:10 +0530

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import RQSuperHeroes from './pages/RQSuperHeroes';
+import SuperHeroes from './pages/SuperHeroes';
 
 function App() {
   return (
-    <div className="App">
-     react query
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/super-heroes' element={ <SuperHeroes /> } />
+          <Route path='/rq-super-heroes' element={ <RQSuperHeroes /> } />
+          <Route path='/' element={ <Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SuperHeroes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SuperHeroes = () => {
+  return <div>Traditional Super Heroes Page</div>;
+};
+
+export default SuperHeroes;
